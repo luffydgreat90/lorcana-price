e090645e834cf60a0f6c6df67ab7e506820ec92c
Marlon Ansale <117570623+luffydgreat90@users.noreply.github.com>
2024-08-18 08:25:22 +0100

    clean card detail view

--- LorcanaFeed/LorcanaSwiftUI/CardDetailUI/Views/CardDetailView.swift
+++ LorcanaFeed/LorcanaSwiftUI/CardDetailUI/Views/CardDetailView.swift
@@ -10,97 +10,87 @@ import LorcanaFeed
 
 struct CardDetailView: View {
     let card: CardViewModel
-    
+    @State var offset: CGSize = .zero
     var body: some View {
         ScrollView(.vertical) {
-            VStack(alignment: .leading) {
-                HStack(alignment: .center, content: {
-                    AsyncImage(url: card.imageNormal,
-                               content: { image in
-                        image.resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: 180, maxHeight: 400)
-                    },
-                               placeholder: {
-                        
-                    })
+            VStack(alignment: .center) {
+                Text(card.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Text(card.version)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                    .foregroundColor(.gray)
+                
+                HStack(alignment: .center, spacing: 16.0) {
+                    Spacer()
+                    
                     VStack(alignment: .leading) {
-                        Text(card.collectorNumber)
+                        Text("Price:")
+                            .font(.caption)
+                            
+                        Text(card.norlamPrice)
                             .font(.title2)
-                            .fontWeight(.semibold)
-                            .padding(.top, 8)
-                        
-                        Text(card.name)
-                            .font(.title2)
-                            .fontWeight(.semibold)
-                           
-                        
-                        Text(card.version)
-                            .font(.footnote)
-                            .multilineTextAlignment(.leading)
-                            .lineLimit(2)
+                    }
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background {
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .fill(.blue)
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("Foil:")
+                            .font(.caption)
                             .foregroundColor(.gray)
-                        
-                        Text(card.rarity)
-                            .font(.subheadline)
-                            .multilineTextAlignment(.leading)
-                            .lineLimit(2)
-                            .padding(.top, 4)
-                        
-                        Text(card.ink)
-                            .font(.subheadline)
-                            .multilineTextAlignment(.leading)
-                            .lineLimit(2)
-                            .padding(.top, 4)
-                        
-                        Text(card.setName)
-                            .font(.subheadline)
-                            .multilineTextAlignment(.leading)
-                            .lineLimit(2)
-                            .padding(.top, 4)
-                        
-                        Spacer()
-                    }.padding(16)
-                })
-                
-                HStack {
+                        Text(card.foilPrice)
+                    }
                     Spacer()
-                    Text(card.norlamPrice)
-               
-                    Text(card.foilPrice)
-                    Spacer()
-                }.padding(.top, 8.0)
+                }.padding(.top, 8)
                 
-                HStack {
-                    Text("Description:")
-                        .font(.subheadline)
-                    Text(card.flavorText)
-                        .font(.subheadline)
-                        .frame(width: .infinity)
-                        .multilineTextAlignment(.leading)
-                }.padding(.top, 8.0)
                 
-                HStack {
-                    Text("Type:")
-                        .font(.subheadline)
-                    Text("Character")
-                        .font(.subheadline)
-                        .frame(width: .infinity)
-                        .multilineTextAlignment(.leading)
-                }.padding(.top, 8.0)
+                AsyncImage(url: card.imageBig,
+                           content: { image in
+                    image.resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: 320, maxHeight: 700)
+                        .rotation3DEffect(offset2Angle(true), axis: (x: -1, y: 0, z: 0))
+                        .rotation3DEffect(offset2Angle(), axis: (x: 0, y: 1, z: 0))
+                        .rotation3DEffect(offset2Angle(true) * 0.1, axis: (x: 0, y: 0, z: 1))
+                },
+                           placeholder: {
+                }).padding(.top, 16)
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                offset = value.translation
+                            }).onEnded({ _ in
+                                withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.32, blendDuration: 0.32)){
+                                    offset = .zero
+                                }
+                            })
+                    )
                 
-//                HStack {
-//                    Text("Illustrator:")
-//                        .font(.subheadline)
-//                    Text(card.)
-//                        .font(.subheadline)
-//                        .frame(width: .infinity)
-//                        .multilineTextAlignment(.leading)
-//                }.padding(.top, 8.0)
-
-            }.padding(8)
+            }
         }
     }
+    
+    // MARK: Converting Offset Into X,Y Angles
+    func offset2Angle(_ isVertical: Bool = false)->Angle{
+        let progress = (isVertical ? offset.height : offset.width) / (isVertical ? screenSize.height : screenSize.width)
+        return .init(degrees: progress * 10)
+    }
+    
+    // MARK: Device Screen Size
+    var screenSize: CGSize = {
+        guard let window = UIApplication.shared.connectedScenes.first as? UIWindowScene else{
+            return .zero
+        }
+        
+        return window.screen.bounds.size
+    }()
 }
 
 #Preview {
